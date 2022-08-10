11b0026a2b5a93e667b41d2448b2d92c0131f511
kata <kata.choi@gmail.com>
2022-08-10 09:48:28 +0800

    use polynomial struct for lagrange MLE

--- src/lagrange.rs
+++ src/lagrange.rs
@@ -1,26 +1,99 @@
+use ark_ff::{Zero, PrimeField};
+use ark_poly::{multivariate::{SparsePolynomial, SparseTerm, Term}, MVPolynomial, Polynomial};
+use crate::small_fields::F5;
+
+
 // One step in chi
-pub fn chi_step(w: bool, x: i128) -> i128 {
-	x * i128::from(w) + (1 - x) * (1 - i128::from(w))
+pub fn chi_step(w: bool, v: usize) -> SparsePolynomial<F5, SparseTerm> {
+	// x * i128::from(w) + (1 - x) * (1 - i128::from(w));
+	// println!("step v+1:{}", v+1);
+
+	let one_minus_w = (1 - (w as i128)).into();
+	// println!("1-w: {}", one_minus_w);
+	let w_minus_one = w as i128 - 1;
+	// println!("w-1: {}", w_minus_one);
+
+	let f: SparsePolynomial<F5, SparseTerm> = SparsePolynomial::from_coefficients_vec(
+		v + 1,
+		vec![
+			//x * i128::from(w)
+			(w.into(), SparseTerm::new(vec![(v, 1)])),
+			// 1 - w
+			(one_minus_w, SparseTerm::new(vec![(0, 0)])),
+			// (-1 + w) * x
+			(w_minus_one.into(), SparseTerm::new(vec![(v, 1)])),
+		],
+	);
+
+	// println!("2nd term coeff: {:?}", f.terms()[1]);
+
+	f
 }
 
 // Converts i into an index in {0,1}^v, used to retrieves f evaluations
 pub fn n_to_vec(i: usize, n: usize) -> Vec<bool> {
-	let x: Vec<bool> = format!("{:0>width$}", format!("{:b}", i), width = n)
-		.chars()
-		.map(|x| x == '1')
+	let x = format!("{:0>width$}", format!("{:b}", i), width = n);
+	// println!("x: {:?}", x);
+	let x: Vec<bool> = x.chars().map(|x| x == '1')
 		.collect();
+	// println!("x: {:?}, i: {:?}, n: {:?}", x, i, n);
 	x
+}
+
+/// Perform a naive n^2 multiplication of `self` by `other`.
+fn naive_mul(
+	cur: &SparsePolynomial<F5, SparseTerm>,
+	other: &SparsePolynomial<F5, SparseTerm>,
+) -> SparsePolynomial<F5, SparseTerm> {
+	if cur.is_zero() || other.is_zero() {
+		// println!("zero");
+		SparsePolynomial::zero()
+	} else {
+		let mut result_terms = Vec::new();
+		for (cur_coeff, cur_term) in cur.terms.iter() {
+			for (other_coeff, other_term) in other.terms.iter() {
+				let mut term: Vec<(usize, usize)> = cur_term.vars().iter().zip(cur_term.powers()).map(|(v, p)| (*v, p)).collect();
+				term.extend(
+					other_term.vars().iter().zip(other_term.powers()).map(|(v, p)| (*v, p))
+				);
+				// let mut term = cur_term.0.clone();
+				// term.extend(other_term.0.clone());
+				let coeff = *cur_coeff * *other_coeff;
+				// println!("naive mul coeff: {}, cur_coeff: {}, other_coeff: {}", coeff, cur_coeff, other_coeff);
+				result_terms.push((coeff, SparseTerm::new(term)));
+			}
+		}
+		let maxvars = core::cmp::max(cur.num_vars, other.num_vars);
+		SparsePolynomial::from_coefficients_slice(maxvars, result_terms.as_slice())
+	}
 }
 
 // Computes Chi_w(r) for all w, O(log n) operations
 pub fn chi_w(w: &Vec<bool>, r: &Vec<i128>) -> i128 {
+	// println!("chi w...");
 	assert_eq!(w.len(), r.len());
-	let product: i128 = w
+	let product: SparsePolynomial<F5, SparseTerm> = w
 		.iter()
-		.zip(r.iter())
-		.map(|(&w, &r)| chi_step(w, r))
-		.product();
-	product
+		.enumerate()
+		.fold(
+			SparsePolynomial::from_coefficients_slice(0, &[(F5::from(1), SparseTerm::new(Vec::new()))]), 
+			|p, (i, w)| {
+				let step = chi_step(*w, i);
+				let m = naive_mul(&p, &step);
+				m
+			}
+		);
+
+	let r: Vec<F5> = r.iter().map(|i| F5::from(*i)).collect();
+
+	let s = &product.evaluate(&r);
+	// println!("{:?}", product.terms()[0].0.into_repr());
+	// println!("{:?}", product.terms()[1].0.into_repr());
+	// println!("{:?}", product.terms()[2].0.into_repr());
+	// println!("{:?}", product.terms()[3].0.into_repr());
+	let result = i128::from_str_radix(&s.into_repr().to_string(), 16).unwrap();
+	// println!("chi w result:{:?}", result);
+	result
 }
 
 // Calculating the slow way, for benchmarking
@@ -31,6 +104,9 @@ pub fn slow_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
 		.enumerate()
 		.map(|(i, val)| val * chi_w(&n_to_vec(i, r.len()), r))
 		.sum();
+	
+	// println!("slow mle sum {}", sum);
+	// println!("sum%p {}", sum % p);
 	sum % p
 }
 
@@ -46,30 +122,30 @@ pub fn recurse(fw: &Vec<i128>, r: &Vec<i128>, n: usize) -> i128 {
 	}
 }
 
-// Lemm 3.8
-pub fn dynamic_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
-	let chi_lookup = memoize(r, r.len());
-	let result: i128 = fw
-		.iter()
-		.zip(chi_lookup.iter())
-		.map(|(left, right)| left * right)
-		.sum();
-	result % p
-}
+// // Lemm 3.8
+// pub fn dynamic_mle(fw: &Vec<i128>, r: &Vec<i128>, p: i128) -> i128 {
+// 	let chi_lookup = memoize(r, r.len());
+// 	let result: i128 = fw
+// 		.iter()
+// 		.zip(chi_lookup.iter())
+// 		.map(|(left, right)| left * right)
+// 		.sum();
+// 	result % p
+// }
 
-pub fn memoize(r: &Vec<i128>, v: usize) -> Vec<i128> {
-	match v {
-		1 => {
-			vec![chi_step(false, r[v - 1]), chi_step(true, r[v - 1])]
-		}
-		_ => memoize(r, v - 1)
-			.iter()
-			.flat_map(|val| {
-				[
-					val * chi_step(false, r[v - 1]),
-					val * chi_step(true, r[v - 1]),
-				]
-			})
-			.collect(),
-	}
-}
+// pub fn memoize(r: &Vec<i128>, v: usize) -> Vec<i128> {
+// 	match v {
+// 		1 => {
+// 			vec![chi_step(false, r[v - 1]), chi_step(true, r[v - 1])]
+// 		}
+// 		_ => memoize(r, v - 1)
+// 			.iter()
+// 			.flat_map(|val| {
+// 				[
+// 					val * chi_step(false, r[v - 1]),
+// 					val * chi_step(true, r[v - 1]),
+// 				]
+// 			})
+// 			.collect(),
+// 	}
+// }
--- tests/test_lagrange.rs
+++ tests/test_lagrange.rs
@@ -17,6 +17,13 @@ lazy_static! {
 	static ref R_5_CHI: Vec<i128> = Vec::from([0, 0, 0, 0, 2, -3, -4, 6]);
 	static ref R_6: Vec<i128> = Vec::from([5, 5, 5]);
 	static ref R_6_CHI: Vec<i128> = Vec::from([-64, 80, 80, -100, 80, -100, -100, 125]);
+
+	static ref F_W: Vec<i128> = Vec::from([
+		0, 0, 0, 0,
+		0, 1, 1, 0,
+		0, 1, 0, 0,
+		0, 0, 0, 0
+	]);
 }
 
 #[rstest]
@@ -47,24 +54,24 @@ fn stream_lagrange_test(
 	assert_eq!(lagrange::stream_mle(fw, r, p), expected);
 }
 
-#[rstest]
-#[case(&F_2, &R_0, 1, 5)]
-#[case(&F_2, &R_1, 3, 5)]
-#[case(&F_2, &R_2, 4, 5)]
-#[case(&F_2, &R_3, 0, 5)]
-fn dynamic_mle_test(
-	#[case] fw: &Vec<i128>,
-	#[case] r: &Vec<i128>,
-	#[case] expected: i128,
-	#[case] p: i128,
-) {
-	assert_eq!(lagrange::dynamic_mle(fw, r, p), expected);
-}
+// #[rstest]
+// #[case(&F_2, &R_0, 1, 5)]
+// #[case(&F_2, &R_1, 3, 5)]
+// #[case(&F_2, &R_2, 4, 5)]
+// #[case(&F_2, &R_3, 0, 5)]
+// fn dynamic_mle_test(
+// 	#[case] fw: &Vec<i128>,
+// 	#[case] r: &Vec<i128>,
+// 	#[case] expected: i128,
+// 	#[case] p: i128,
+// ) {
+// 	assert_eq!(lagrange::dynamic_mle(fw, r, p), expected);
+// }
 
-#[rstest]
-#[case(&R_4, &R_4_CHI)]
-#[case(&R_5,&R_5_CHI)]
-#[case(&R_6, &R_6_CHI)]
-fn memoize_test(#[case] r: &Vec<i128>, #[case] expected: &Vec<i128>) {
-	assert_eq!(lagrange::memoize(r, r.len()), *expected);
-}
+// #[rstest]
+// #[case(&R_4, &R_4_CHI)]
+// #[case(&R_5,&R_5_CHI)]
+// #[case(&R_6, &R_6_CHI)]
+// fn memoize_test(#[case] r: &Vec<i128>, #[case] expected: &Vec<i128>) {
+// 	assert_eq!(lagrange::memoize(r, r.len()), *expected);
+// }
