d857fe5674b0f11f92d84336087afb8392b64f44
kata <kata.choi@gmail.com>
2022-08-14 15:26:29 +0800

    sumcheck for triangle counting

--- tests/test_sumcheck.rs
+++ tests/test_sumcheck.rs
@@ -4,7 +4,8 @@ extern crate lazy_static;
 use ark_poly::polynomial::multivariate::{SparsePolynomial, SparseTerm, Term};
 use ark_poly::polynomial::MVPolynomial;
 use rstest::rstest;
-use thaler::small_fields::F251;
+use thaler::lagrange::poly_count_triangles;
+use thaler::small_fields::{F251};
 use thaler::sumcheck;
 
 lazy_static! {
@@ -29,11 +30,21 @@ lazy_static! {
 		],
 	);
 	static ref G_1_SUM: F251 = sumcheck::Prover::new(&G_1).slow_sum_g();
+
+	static ref G_2: sumcheck::MultiPoly = poly_count_triangles(&Vec::from([
+		0, 1, 1, 0,
+		1, 0, 1, 1,
+		1, 1, 0, 1,
+		0, 1, 1, 0
+	]));
+	static ref G_2_SUM: F251 = sumcheck::Prover::new(&G_2).slow_sum_g();
+
 }
 
 #[rstest]
 #[case(&G_0, &G_0_SUM)]
 #[case(&G_1, &G_1_SUM)]
+#[case(&G_2, &G_2_SUM)]
 fn sumcheck_test(#[case] p: &sumcheck::MultiPoly, #[case] c: &F251) {
 	assert!(sumcheck::verify(&p, *c));
 }
