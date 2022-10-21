78368e0552ab62377f2319d012a5866e52ab2729
kata <kata.choi@gmail.com>
2022-10-21 14:12:02 +0800

    bench different mle methods

--- benches/bench_sumcheck.rs
+++ benches/bench_sumcheck.rs
@@ -1,8 +1,8 @@
 use std::rc::Rc;
 use thaler::small_fields::{F251};
 use thaler::sumcheck::{self, SumCheckPolynomial, Prover, UniPoly, Verifier};
-use thaler::triangles::Triangles;
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use thaler::triangles::{Triangles, PolynomialEvalType, Matrix};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId, BenchmarkGroup};
 
 // a gi lookup table
 fn build_gi_lookup(g: &Triangles<F251>) -> Vec<sumcheck::UniPoly> {
@@ -19,76 +19,80 @@ fn build_gi_lookup(g: &Triangles<F251>) -> Vec<sumcheck::UniPoly> {
 	lookup
 }
 
-fn bench_verifier(c: &mut Criterion) {
+fn bench_verifier_steps(c: &mut Criterion) {
 	let mut group = c.benchmark_group("verifier");
 
 	let matrix_sizes = [4, 8, 16, 32];
+	let eval_types = &[
+		PolynomialEvalType::SLOW_MLE,
+		PolynomialEvalType::DYNAMIC_MLE,
+		PolynomialEvalType::STREAM_MLE,
+	];
 	for size in matrix_sizes {
-		let matrix: Vec<Vec<F251>> = thaler::utils::gen_matrix(size);
-		let g: Triangles<F251> = thaler::triangles::Triangles::new(matrix.clone());
-		let p: Prover<Triangles<F251>> = sumcheck::Prover::new(&g.clone());
-		let g_sum = p.slow_sum_g();
-		let lookup = build_gi_lookup(&g);
-	
-		let mut v: Verifier<UniPoly, Triangles<F251>> = Verifier::new(g_sum, Rc::new(g.clone()));
-		v.random_func(|| F251::from(1));
-		
-		//todo figure an easy way to add bench for different algo implementations
+		let matrix = Matrix::new(thaler::utils::gen_matrix(size));
+
+		let g: Triangles<F251> = matrix.derive_mle(PolynomialEvalType::DYNAMIC_MLE);
 		group.bench_function(
-			BenchmarkId::new::<&str, usize>("verifier with vars", lookup.len()), 
-			|b| {
-				b.iter(|| {
-					let mut v = v.clone();
-					(0..lookup.len()).for_each(|i| {
-						let gi = lookup[i].clone();
-						v.verify(Some(gi))
-					});
-					v.verify(None);
-				});
-			}
-		);
-		group.bench_function(
-			BenchmarkId::new::<&str, usize>("count triangles with size", lookup.len()), 
+			BenchmarkId::new::<&str, usize>("count triangles with size", g.num_vars()), 
 			|b| {
 				b.iter(|| {
 					g.count()
 				});
 			}
 		);
+
+		for eval_type in eval_types {
+			let g: Triangles<F251> = matrix.derive_mle(eval_type.clone());
+			let p: Prover<Triangles<F251>> = sumcheck::Prover::new(&g.clone());
+			let g_sum = p.slow_sum_g();
+			let lookup = build_gi_lookup(&g);
+		
+			let mut v: Verifier<UniPoly, Triangles<F251>> = Verifier::new(g_sum, Rc::new(g.clone()));
+			v.random_func(|| F251::from(1));
+			
+			group.bench_function(
+				BenchmarkId::new::<&str, usize>(&format!("verifier with eval {:?} in vars", eval_type), lookup.len()), 
+				|b| {
+					b.iter(|| {
+						let mut v = v.clone();
+						(0..lookup.len()).for_each(|i| {
+							let gi = lookup[i].clone();
+							v.verify(Some(gi))
+						});
+						v.verify(None);
+					});
+				}
+			);
+		}
 	}
 
+	group.finish()
+}
 
+fn benchmarks(c: &mut Criterion) {
+	bench_verifier_steps(c);
+
+	// bench_prover_lookup_build(c);
+}
+
+fn bench_prover_lookup_build(c: &mut Criterion) {
+	let mut group = c.benchmark_group("prover");
+
+	let matrix_sizes = [4];
+	for size in matrix_sizes {
+		let matrix = Matrix::new(thaler::utils::gen_matrix(size));
+		let g: Triangles<F251> = matrix.derive_mle(PolynomialEvalType::DYNAMIC_MLE);
+		group.bench_function(
+			BenchmarkId::new::<&str, usize>("prover for size", size), 
+			|b| {
+				b.iter(|| {
+					build_gi_lookup(&g);
+				});
+			}
+		);
+	}
 	group.finish();
 }
 
-// #[bench]
-// fn prover_lookup_build(b: &mut Bencher) {
-// 	b.iter(|| {
-// 		build_gi_lookup();
-// 	});
-// }
-
-// #[bench]
-// fn sumcheck_last_round_test(b: &mut Bencher) {
-// 	let r: Option<F251> = Some(2u32.into());
-	
-// 	b.iter(|| {
-// 		G_1.evaluate(&vec![r.unwrap(); G_1.num_vars()]);
-// 	});
-// }
-
-// #[bench]
-// fn slow_sumcheck_test(b: &mut Bencher) {
-// 	let p = sumcheck::Prover::<Triangles>::new(&G_1);
-// 	b.iter(|| p.slow_sum_g());
-// }
-
-// #[bench]
-// fn naive_count(b: &mut Bencher) {
-// 	b.iter(|| {
-// 		G_1.count();
-// 	});
-// }
-
-criterion_group!(benches, bench_verifier);
+criterion_group!(benches, benchmarks);
 criterion_main!(benches);
